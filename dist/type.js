1bcd8c530b830a73532f11dcdb82b95107d8f9e1
Matthew Phillips <matthew@matthewphillips.info>
2013-11-25 07:50:10 -0500

    Adding a build version.

---
+++ dist/type.js
@@ -0,0 +1,42 @@
+;(function(){
+require.register("type/index.js", function(exports, require, module){
+
+/**
+ * toString ref.
+ */
+
+var toString = Object.prototype.toString;
+
+/**
+ * Return the type of `val`.
+ *
+ * @param {Mixed} val
+ * @return {String}
+ * @api public
+ */
+
+module.exports = function(val){
+  switch (toString.call(val)) {
+    case '[object Function]': return 'function';
+    case '[object Date]': return 'date';
+    case '[object RegExp]': return 'regexp';
+    case '[object Arguments]': return 'arguments';
+    case '[object Array]': return 'array';
+    case '[object String]': return 'string';
+  }
+
+  if (val === null) return 'null';
+  if (val === undefined) return 'undefined';
+  if (val && val.nodeType === 1) return 'element';
+  if (val === Object(val)) return 'object';
+
+  return typeof val;
+};
+
+});if (typeof exports == "object") {
+  module.exports = require("type");
+} else if (typeof define == "function" && define.amd) {
+  define(function(){ return require("type"); });
+} else {
+  this["type"] = require("type");
+}})();
